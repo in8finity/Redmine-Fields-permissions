c9ccc64e16f6a1ade41324c3cb069009bb483c1b
Alexander Morozov <morozov.18.09@gmail.com>
2016-05-14 17:14:49 +0300

    Support for Redmine 2.x

--- init.rb
+++ init.rb
@@ -8,19 +8,32 @@ Redmine::Plugin.register :redmine_fields_permissions do
   url 'http://www.sysdream.com/'
   author_url 'http://www.sysdream.com/'
 
-  # Permissions for assigned to field
-  permission :edit_assigned_to, :issues => :index
-  
-  # Permissions for due date field
-  permission :edit_due_date, :issues => :index
-  
-  # Permissions for estimated hours field
-  permission :edit_estimated_hours, :issues => :index
+  Redmine::AccessControl.map do |map|
+    map.project_module :issue_tracking do |map|
+        #map.permission :set_issues_private, {}
+        #map.permission :add_issues, {:issues => [:new, :create, :update_form], :attachments => :upload}
+      # Permissions for assigned to field
+      map.permission :edit_issue_assigned_to, {:issues => [:new, :create, :update_form]}
+      
+      # Permissions for due date field
+      map.permission :edit_issue_due_date, {:issues => [:new, :create, :update_form]}
+      
+      # Permissions for estimated hours field
+      map.permission :edit_issue_estimated_hours, {:issues => [:new, :create, :update_form]}
 
-  # Permissions for priority field
-  permission :edit_priority, :issues => :index
+      # Permissions for priority field
+      map.permission :edit_issue_priority, {:issues => [:new, :create, :update_form]}
 
-  # Permissions for fixed version field
-  permission :edit_fixed_version, :issues => :index
+      # Permissions for priority field
+      map.permission :edit_issue_status, {:issues => [:new, :create, :update_form]}
+
+      # Permissions for fixed version field
+      map.permission :edit_issue_fixed_version, {:issues => [:new, :create, :update_form]}
+
+      map.permission :edit_issue_category, {:issues => [:new, :create, :update_form]}
+
+      map.permission :edit_issue_done_ratio, {:issues => [:new, :create, :update_form]}
+    end
+  end
 end
 
